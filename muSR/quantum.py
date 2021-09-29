161a4eb200c87b92c5c76eaa383b7bdfe229b2c8
James <jago.urquhart@gmail.com>
2021-09-29 11:38:04 +0100

    Moved original files to a subfolder.

---
+++ muSR/quantum.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def partial_trace2(density_matrix: np.ndarray, index: int) -> np.ndarray:
+    """
+    Takes an operator density_matrix belonging to the 2**N dimensional Hilbert space
+    formed from the tensor product of 2 dimensional subspaces, 0,...,N.
+
+    Returns the partial trace over all subspaces other than index.
+    """
+    N = int(np.log2(density_matrix.shape[0]))
+    if index != 0:
+        density_matrix = np.moveaxis(
+            density_matrix.reshape(np.full(2*N, 2)),
+            (0, index, N, N+index),
+            (index, 0, N+index, N)
+        )
+    return density_matrix.reshape(2, 2**(N-1), 2, 2**(N-1)).trace(axis1=1, axis2=3)
